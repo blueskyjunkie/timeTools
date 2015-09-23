97836b07fe194ac86ded301fd6ab78753a83383e
Russell <blueskyjunkie@users.noreply.github.com>
2015-09-22 21:40:30 -0400

    Config files for pipy

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'timeTools',
+  packages = ['timeTools'], # this must be the same as the name above
+  version = '0.4',
+  description = 'Python utilities relating to synchronization analysis and visualization',
+  author = 'Russell Smiley',
+  author_email = 'im.russell.smiley@gmail.com',
+  url = 'https://github.com/blueskyjunkie/timeTools', # use the URL to the github repo
+  download_url = 'https://zenodo.org/record/15797/files/timeTools-v0.4.zip', # I'll explain this in a second
+  keywords = [ 'synchronization', 'visualization', 'analysis', 'PDV', 'MTIE', 'TDEV' ], # arbitrary keywords
+  classifiers = [],
+)
