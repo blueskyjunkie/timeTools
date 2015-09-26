aa662cf8c6293cab47c72362c0d990cae779a777
Russell <blueskyjunkie@users.noreply.github.com>
2015-09-26 08:10:37 -0400

    Align project structure with python conventions

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 setup(
-  name = 'timeTools',
-  packages = ['timeTools'], # this must be the same as the name above
+  name = 'timetools',
+  packages = ['timetools'], # this must be the same as the name above
   version = '0.4',
   description = 'Python utilities relating to synchronization analysis and visualization',
   author = 'Russell Smiley',
   author_email = 'im.russell.smiley@gmail.com',
-  url = 'https://github.com/blueskyjunkie/timeTools', # use the URL to the github repo
+  url = 'https://github.com/blueskyjunkie/timetools', # use the URL to the github repo
   download_url = 'https://zenodo.org/record/15797/files/timeTools-v0.4.zip', # I'll explain this in a second
   keywords = [ 'synchronization', 'visualization', 'analysis', 'PDV', 'MTIE', 'TDEV' ], # arbitrary keywords
   classifiers = [],
